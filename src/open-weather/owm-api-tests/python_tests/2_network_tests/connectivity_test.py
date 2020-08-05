0da7ef9d3cd11a4ddd7240d9f59bee0bd95130f0
shiraztahir <shiraztahir@gmail.com>
2020-08-05 10:58:02 +0100

    * add exception handling while fetching data from server * assert failure when internet connectivity is not availalbe

--- src/open-weather/owm-api-tests/python_tests/2_network_tests/connectivity_test.py
+++ src/open-weather/owm-api-tests/python_tests/2_network_tests/connectivity_test.py
@@ -7,21 +7,20 @@ A set of tests that check whether we can access the internet and API server
 """
 # ============================================================================
 
-import warnings
 import subprocess
 
 
 def test_ping_internet():
     """Test that internet connectivity is available."""
     command = ['ping', "google.com"]
-    if (subprocess.call(command) == 1):
-        warnings.warn("Cannot ping google. Looks like"
-                      "you dont have an internet connection")
+    assert (subprocess.call(command) == 0), \
+        "Cannot ping google. Looks like " \
+        "you dont have an internet connection"
 
 
 def test_ping_OWM():
     """Test OWM web server is not down"""
     command = ['ping', "openweathermap.org"]
-    if (subprocess.call(command) == 1):
-        warnings.warn("Cannot ping OWM. If internet connectivity is fine, "
-                      "OWM server may be down")
+    assert (subprocess.call(command) == 0), \
+        "Cannot ping OWM. If internet connectivity is fine, " \
+        "OWM server may be down"
